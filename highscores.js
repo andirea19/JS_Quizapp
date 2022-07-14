47b839037c1bdcac7cc5cad14277732da7c0c413
andirea19 <ungerer.andrea@gmail.com>
2022-07-14 14:56:40 +0200

    added css to highscore

---
+++ highscores.js
@@ -0,0 +1,17 @@
+const highScoreList = document.getElementById('high-scores-list');
+
+//hier mit Max? oder auf der Seite
+const MAX_HIGHSCORES = 3;
+const highScores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+//console.log(highScores);
+
+highScoresList.innerHTML = 'Highscores:<br>'.HighScores;
+
+highScores.map(score => {
+    console.log( '<li class="list-group-item">' + score.name + ': ' + score.score + '</li>');
+// map zum Umwandeln in eine Liste
+//    highScoreList.innerHTML += '<li class="list-group-item">' + score.name + ': ' + score.score + '</li>';
+    .join('');
+}
+);
